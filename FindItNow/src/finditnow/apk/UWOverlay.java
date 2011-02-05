9fc00a211854030178e120ee2b6398fab553975b
Dustin <Dustin@localhost>
2011-02-04 22:09:05 -0800

    Modified our overlay on tap command to perform an httppost to our php server and return the string retrieved from the database and display it on the dialog popup in the emulator. You should now see 5 geopoint (lattitude and longitude coordinates) when you click on the emulator.

--- FindItNow/src/finditnow/apk/UWOverlay.java
+++ FindItNow/src/finditnow/apk/UWOverlay.java
@@ -1,10 +1,28 @@
 package finditnow.apk;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
+
 import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.OverlayItem;
 
@@ -42,8 +60,57 @@ public class UWOverlay extends ItemizedOverlay<OverlayItem> {
 	protected boolean onTap(int index) {
 	  OverlayItem item = mOverlays.get(index);
 	  AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+	  
+	  /*
+	   * HTTP Post request
+	   */
+	String result = "";
+	InputStream is = null;
+	try{
+	        HttpClient httpclient = new DefaultHttpClient();
+	        HttpPost httppost = new HttpPost("http://cubist.cs.washington.edu/~dustinab/index.php");
+			//httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+	        HttpResponse response = httpclient.execute(httppost);
+	        HttpEntity entity = response.getEntity();
+	        is = entity.getContent();
+	}catch(Exception e){
+	        Log.e("log_tag", "Error in http connection "+e.toString());
+	}
+	//convert response to string
+	try{
+	        BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
+	        StringBuilder sb = new StringBuilder();
+	        String line = null;
+	        while ((line = reader.readLine()) != null) {
+	                sb.append(line + "\n");
+	        }
+	        is.close();
+	 
+	        result=sb.toString();
+	}catch(Exception e){
+	        Log.e("log_tag", "Error converting result "+e.toString());
+	}
+	 
+	//reference for converting to and parsing json data
+	/*
+	try{
+        JSONArray jArray = new JSONArray(result);
+        for(int i=0;i<jArray.length();i++){
+            JSONObject json_data = jArray.getJSONObject(i);
+            Log.i("log_tag","id: "+json_data.getInt("id")+
+                ", name: "+json_data.getString("name")+
+                ", sex: "+json_data.getInt("sex")+
+                ", birthyear: "+json_data.getInt("birthyear")
+            );
+        }
+	} catch(JSONException e){
+	        Log.e("log_tag", "Error parsing data "+e.toString());
+	}
+	*/
+	  
 	  dialog.setTitle(item.getTitle());
-	  dialog.setMessage(item.getSnippet());
+	  //dialog.setMessage(item.getSnippet());
+	  dialog.setMessage(result);
 	  dialog.show();
 	  return true;
 	}
