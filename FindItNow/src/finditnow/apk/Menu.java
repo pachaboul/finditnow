168baa2352aff3633c71bf8afb3748dcc3962256
dangstar <dangstar@localhost>
2011-02-12 16:56:53 -0800

    Redid menu layout into a gridview, with default icons for now

--- FindItNow/src/finditnow/apk/Menu.java
+++ FindItNow/src/finditnow/apk/Menu.java
@@ -1,10 +1,17 @@
 package finditnow.apk;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Menu extends Activity {
 	
@@ -12,54 +19,66 @@ public class Menu extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.menu);
 		
-		// They all jump to the map for now
-		Button buildings = (Button) findViewById(R.id.BuildingsButton);
-		onClickToCategory(buildings, "building");
-		
-		Button vending = (Button) findViewById(R.id.VendingButton);
-		onClickToCategory(vending, "vending");
-		
-		Button restrooms = (Button) findViewById(R.id.RestroomsButton);
-		onClickToCategory(restrooms, "restrooms");
-		
-		Button coffee = (Button) findViewById(R.id.CoffeeButton);
-		onClickToCategory(coffee, "coffee");
-		
-		Button recycling = (Button) findViewById(R.id.RecyclingButton);
-		onClickToCategory(recycling, "recycling");
-		
-		Button dining = (Button) findViewById(R.id.DiningButton);
-		onClickToCategory(dining, "dining");
-		
-		Button supplies = (Button) findViewById(R.id.SuppliesButton);
-		onClickToCategory(supplies, "supplies");
-		
-		Button atms = (Button) findViewById(R.id.ATMButton);
-		onClickToCategory(atms, "ATMs");
+        GridView buttonGrid = (GridView) findViewById(R.id.gridview);
+        buttonGrid.setAdapter(new ButtonAdapter(this));
+	}
 
-	}
-	
-	// Adds a click listener to a button, jumps to Map class.
-	// (maybe there's a better way to pass the category string within the inner function.
-	// Write a different listener?)
-	private void onClickToCategory(Button b, String category) {
-		b.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), Map.class);
-                // myIntent.putExtra("category", category);
-                startActivityForResult(myIntent, 0);
-            }
-        });
-	}
-	/*
-	private void onClickToList(Button b, String category) {
-		b.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), TypeList.class);
-                // myIntent.putExtra("category", category);
-                startActivityForResult(myIntent, 0);
-            }
-        });  
-	}
-	*/
+	// This class/list feeds into the grid view.
+	public class ButtonAdapter extends BaseAdapter {
+    	private Context mContext;
+    	
+        private String[] mButtonText = {
+            "ATMs",
+            "Buildings",
+            "Coffee",
+            "Dining",
+            "Recycling",
+            "Restrooms",
+            "Supplies",
+            "Vending"
+        };
+    	
+    	public ButtonAdapter(Context c) {
+    		mContext = c;
+    	}
+
+    	public int getCount() {
+    		return 8;
+    	}
+
+    	public Object getItem(int position) {
+    		return null;
+    	}
+
+    	public long getItemId(int position) {
+    		return 0;
+    	}
+
+    	public View getView(int position, View convertView, ViewGroup parent) {
+    		View myView = convertView;
+    		
+			// Define the view that appears in each grid cell.
+    		if (convertView == null) {	
+    			// inflate the layout
+    			LayoutInflater li = getLayoutInflater();
+    			myView = li.inflate(R.layout.grid_item, null);
+    			
+    			// add image button
+    			ImageButton ib = (ImageButton) myView.findViewById(R.id.grid_item_button);
+    			ib.setImageResource(R.drawable.android);
+    			ib.setOnClickListener(new OnClickListener() {
+					public void onClick(View v) {
+						Intent myIntent = new Intent(v.getContext(), Map.class);
+		                // myIntent.putExtra("category", category);
+		                startActivity(myIntent);
+					}
+    			});
+    			
+    			// add text
+    			TextView tv = (TextView) myView.findViewById(R.id.grid_item_text);
+    			tv.setText(mButtonText[position]);
+    		}
+    		return myView;
+    	}
+    }
 }
