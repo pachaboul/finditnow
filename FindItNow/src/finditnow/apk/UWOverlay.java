deacdd56b00e2e0f571fdad0380b533812670eb2
Raftysworld <Raftysworld@localhost>
2011-02-18 00:47:28 -0800

    Retrieve location information from PHP file, detect building name from GeoPoint coordinates, etc.

--- FindItNow/src/finditnow/apk/UWOverlay.java
+++ FindItNow/src/finditnow/apk/UWOverlay.java
@@ -47,69 +47,19 @@ public class UWOverlay extends ItemizedOverlay<OverlayItem> {
 	
 	@Override
 	protected boolean onTap(int index) {
-	  OverlayItem item = mOverlays.get(index);
-	  AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+		OverlayItem item = mOverlays.get(index);
+		GeoPoint itemLocation = item.getPoint();
+	    //This is how you use a PopUpDialog
+	    String[] floor = {"Level 6", "Level 5", "Level 4", "Level 3","Level 2","Level 1","Levev B"};
 	  
-	  /*
-	   * HTTP Post request
-	   */
-	String result = "";
-	InputStream is = null;
-	try{
-	        HttpClient httpclient = new DefaultHttpClient();
-	        HttpPost httppost = new HttpPost("http://cubist.cs.washington.edu/~dustinab/index.php");
-			//httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-	        HttpResponse response = httpclient.execute(httppost);
-	        HttpEntity entity = response.getEntity();
-	        is = entity.getContent();
-	}catch(Exception e){
-	        Log.e("log_tag", "Error in http connection "+e.toString());
-	}
-	//convert response to string
-	try{
-	        BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
-	        StringBuilder sb = new StringBuilder();
-	        String line = null;
-	        while ((line = reader.readLine()) != null) {
-	                sb.append(line + "\n");
-	        }
-	        is.close();
-	 
-	        result=sb.toString();
-	}catch(Exception e){
-	        Log.e("log_tag", "Error converting result "+e.toString());
-	}
-	 
-	//reference for converting to and parsing json data
-	/*
-	try{
-        JSONArray jArray = new JSONArray(result);
-        for(int i=0;i<jArray.length();i++){
-            JSONObject json_data = jArray.getJSONObject(i);
-            Log.i("log_tag","id: "+json_data.getInt("id")+
-                ", name: "+json_data.getString("name")+
-                ", sex: "+json_data.getInt("sex")+
-                ", birthyear: "+json_data.getInt("birthyear")
-            );
-        }
-	} catch(JSONException e){
-	        Log.e("log_tag", "Error parsing data "+e.toString());
-	}
-	*/	  
+	    String buildingName = Map.getBuilding(itemLocation);
+	    if (buildingName == null) {
+	  	    buildingName = "Unknown Building";
+	    }
+	    PopUpDialog popUp = new PopUpDialog(mContext, floor, buildingName);
 	  
-	  //dialog.setTitle(item.getTitle());
-	  //dialog.setMessage(item.getSnippet());
-	  //dialog.setMessage(result);
-	  //dialog.show();
-	  
-	  //This is how you use a PopUpDialog
-	  String[] floor = {"Level 6", "Level 5", "Level 4", "Level 3","Level 2","Level 1","Levev B"};
-	  String buildingName = "CSE Building";
-      PopUpDialog popUp = new PopUpDialog(mContext, floor, buildingName);
-	  
-	  popUp.show();
-	  
-	  return true;
+	    popUp.show();
+	    return true;
 	}
 
 }
