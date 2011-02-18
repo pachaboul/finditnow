63d8d1d33bf90f53e7fdb7cbdd295fe62571483e
johnsj8 <johnsj8@cs.washington.edu>
2011-02-17 19:11:42 -0800

    Commiting changes to Map.java that now includes a HTTP POST request to jillyn's test php.

--- FindItNow/src/finditnow/apk/Map.java
+++ FindItNow/src/finditnow/apk/Map.java
@@ -1,71 +1,68 @@
-/** Map.java
- *  This is MapView class which uses the Google Maps API
- *  It draws the overlay, communicates with the database, and detects user location
- */
-
 package finditnow.apk;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import com.google.android.maps.*;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Map extends MapActivity {
 	
-	// Map and Location Variables
 	private MapView mapView;
 	private MapController mapController;
 	private MyLocationOverlay locOverlay;
 	
-	// Overlay Variables
 	private List<Overlay> mapOverlays;
 	private Drawable drawable;
 	private UWOverlay itemizedOverlay;
-	
-	// Shared static variables that the other modules can access
 	private static HashMap<String, Integer> icons;
 	private static String category;
+	private JSONArray listOfLocations;
 	
-    /** Called when the activity is first created.
-     * 	It initializes the map layout, detects the user's category, and builds the map
-     */
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	
     	// Restore the saved instance and generate the primary (main) layout
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.map);
+        setContentView(R.layout.main);
         
         // And to get the item name for buildings, supplies:
         // String itemName = extras.getString("itemName");
         Bundle extras = getIntent().getExtras(); 
         category = extras.getString("category");
         
-        // Store a map from categories to icons so that other modules can use it
         icons = createIconsList();
         
-        // Create the map and detect the user's location
         createMap();
         locateUser();
+        listOfLocations = requestLocations();
     }
     
-    /** This method returns a map from categories to icons (icons must be the same name as the category, in lowercase */
     private HashMap<String, Integer> createIconsList() {
-    	
     	HashMap<String, Integer> iconsMap = new HashMap<String, Integer>();
-    	
-    	// Loop over each category and map it to the icon file associated with it
     	for (String str : Menu.categories) {
 			iconsMap.put(str.toLowerCase(), getResources().getIdentifier("drawable/"+str.toLowerCase(), null, getPackageName()));
 		}
-    	
 		return iconsMap;
     }
     
-    /** This method creates the map and displays the overlays on top of it */
     private void createMap() {
-    	
         // Initialize our MapView and MapController
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
@@ -87,9 +84,7 @@ public class Map extends MapActivity {
         mapOverlays.add(itemizedOverlay);
     }
     
-    /** This method locates the user and displays the user's location in an overlay icon */
     private void locateUser() {
-    	
     	// Define a new LocationOverlay and enable it
         locOverlay = new MyLocationOverlay(this, mapView);
         locOverlay.enableMyLocation();
@@ -106,18 +101,64 @@ public class Map extends MapActivity {
 		locOverlay.runOnFirstFix(runnable);
     }
     
-    /** Required for Android Maps API compatibility */
+    //This method makes a request across the network to the database sending
+    //the current location and category
+    //Returns: a JSONArray if item locations sent from the database
+    private JSONArray requestLocations() {
+    	/*
+  	   * HTTP Post request
+  	   */
+    	String data = "";
+	  	InputStream iStream = null;
+	  	JSONArray infoArray = null;
+	  	try{
+  	        HttpClient httpclient = new DefaultHttpClient();
+  	        HttpPost httppost = new HttpPost("http://cubist.cs.washington.edu/~johnsj8/testJSON.php");
+  			List nameValuePairs = new ArrayList();
+  			
+  			nameValuePairs.add(new BasicNameValuePair("cat", category));
+  			GeoPoint location = locOverlay.getMyLocation();
+  			nameValuePairs.add(new BasicNameValuePair("lat", location.getLatitudeE6()+""));
+  			nameValuePairs.add(new BasicNameValuePair("lon", location.getLongitudeE6()+""));
+  	        
+  			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+  	        HttpResponse response = httpclient.execute(httppost);
+  	        HttpEntity entity = response.getEntity();
+  	        iStream = entity.getContent();
+	  	}catch(Exception e){
+	  	    Log.e("log_tag", "Error in http connection "+e.toString());
+	  	}
+	  	//convert response to string
+	  	try{
+  	        BufferedReader reader = new BufferedReader(new InputStreamReader(iStream,"iso-8859-1"),8);
+  	        StringBuilder sb = new StringBuilder();
+  	        String line = null;
+  	        while ((line = reader.readLine()) != null) {
+  	        	sb.append(line + "\n");
+  	        }
+  	        iStream.close();
+  	 
+  	        data = sb.toString();
+	  	}catch(Exception e){
+	  	    Log.e("log_tag", "Error converting result "+e.toString());
+	  	}
+	  	try {
+			infoArray = new JSONArray(data);
+		} catch (JSONException e) {
+			Log.e("log_tag", "Error converting response to JSON "+e.toString());
+		}
+	  	return infoArray;
+    }
+ 
     @Override
 	protected boolean isRouteDisplayed() {
 		return false;
 	}
     
-    /** This method returns the icons map */
     public static HashMap<String, Integer> getIcons() {
     	return icons;
     }
     
-    /** This method returns the current category */
     public static String getCategory() {
     	return category;
     }
