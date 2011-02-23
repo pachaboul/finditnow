c603ae63cce81f64a8e20a0d0168a071a353dd99
dangstar <dangstar@localhost>
2011-02-22 21:29:57 -0800

    Moar comments

--- FindItNow/src/finditnow/apk/CategoryList.java
+++ FindItNow/src/finditnow/apk/CategoryList.java
@@ -1,3 +1,11 @@
+/*
+ * This class provides the intermediate list screen
+ * that is displayed for Buildings and Supplies.
+ * Selection of any option will launch the map class.
+ * 
+ * Auto-completion suggestions are enabled.
+ */
+
 package finditnow.apk;
 
 import android.app.ListActivity;
@@ -12,36 +20,39 @@ import android.widget.TextView;
 
 public class CategoryList extends ListActivity {
 	
+	// On launch, determine which category type was passed
+	// and display the appropriate list.
+	// Defaults to supplies if category is unrecognized.
     public void onCreate(Bundle savedInstanceState) {
-    	  super.onCreate(savedInstanceState);
-
-    	  Bundle extras = getIntent().getExtras(); 
-    	  final String category = extras.getString("category");
-    	  String[] data;
-    	  
-    	  // Grab the correct list to show.
-    	  if (category.equals("buildings")) {
-    		  data = BUILDINGS;
-    	  } else {
-    		  data = SUPPLIES;
-    	  }
-    	  
-    	  setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, data));
-
-    	  ListView lv = getListView();
-    	  lv.setTextFilterEnabled(true);
-
-    	  lv.setOnItemClickListener(new OnItemClickListener() {
-    		  public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-    			  Intent myIntent = new Intent(v.getContext(), Map.class);
-    			  myIntent.putExtra("category", category);
-    			  myIntent.putExtra("itemName", ((TextView) v).getText());
-    			  startActivity(myIntent);
-    		  }
-    	  });
+    	super.onCreate(savedInstanceState);
+    	
+    	Bundle extras = getIntent().getExtras(); 
+    	final String category = extras.getString("category");
+    	String[] data;
+    	
+    	// Grab the correct list to show.
+    	if (category.equals("buildings")) {
+    		data = BUILDINGS;
+    	} else {
+    		data = SUPPLIES;
     	}
+    	
+    	setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, data));
+    	
+    	ListView lv = getListView();
+    	lv.setTextFilterEnabled(true);
+    	
+    	lv.setOnItemClickListener(new OnItemClickListener() {
+    		public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+    			Intent myIntent = new Intent(v.getContext(), Map.class);
+    			myIntent.putExtra("category", category);
+    			myIntent.putExtra("itemName", ((TextView) v).getText());
+    			startActivity(myIntent);
+    		}
+    	});
+    }
     
-    // Contains priority 1,2 buildings for now.
+    // List of buildings we have data collection for.
     private static final String[] BUILDINGS = new String[] {
     	"Electrical Engineering",
     	"Guggenheim Hall",
@@ -53,8 +64,9 @@ public class CategoryList extends ListActivity {
     	"Smith Hall"
 	};
 	 
-	 private static final String[] SUPPLIES = {
-		 "Blue books",
-		 "Scantrons"
-	 };
+    // List of supplies item types.
+	private static final String[] SUPPLIES = {
+		"Blue books",
+		"Scantrons"
+	};
 }
