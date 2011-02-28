25db7e6f086dbab41011f8d33805ad9afab5ccc0
mfalcone72 <mfalcone72@gmail.com>
2011-02-28 14:19:07 -0800

    Actually added the FINAddNew.java class this time (hopefully)

---
+++ FindItNow/src/com/net/finditnow/FINAddNew.java
@@ -0,0 +1,34 @@
+package com.net.finditnow;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class FINAddNew extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+	}
+
+    protected void onStart() {
+    	
+    }
+    
+    protected void onRestart() {
+    	
+    }
+
+    protected void onResume() {
+    	
+    }
+
+    protected void onPause() {
+    	
+    }
+
+    protected void onStop() {
+    	
+    }
+
+    protected void onDestroy() {
+    	
+    }
+}
