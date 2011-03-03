180d4dde9c8f49f151e3d4e1b2ee2809f2a66609
Mai Dang <Mai Dang@localhost>
2011-03-03 15:26:43 -0800

    added ATMs hack for capFirstChar()

--- FindItNow/src/com/net/finditnow/FINUtil.java
+++ FindItNow/src/com/net/finditnow/FINUtil.java
@@ -6,16 +6,20 @@ import java.util.ArrayList;
  * This is a class that holds some util methods that is shared between classes.
  */
 public class FINUtil {
+	
 	//precondition: str is not empty
 	//post condition: str with first character capitalized
 	public static String capFirstChar(String str)
 	{
-		StringBuffer buffer = new StringBuffer();
-    	buffer.append(str);    	
-    	//char cateName = Character.toUpperCase(buffer.charAt(0));
-    	buffer.setCharAt(0, Character.toUpperCase(buffer.charAt(0)));
-		
-    	return buffer.toString();
+		if (str.equals("atms")) {
+			return "ATMs";
+		} else {
+			StringBuffer buffer = new StringBuffer();
+	    	buffer.append(str);    	
+	    	//char cateName = Character.toUpperCase(buffer.charAt(0));
+	    	buffer.setCharAt(0, Character.toUpperCase(buffer.charAt(0)));
+	    	return buffer.toString();
+		}
 	}
 	
 	public static ArrayList<String> capFirstChar(ArrayList<String> strs)
