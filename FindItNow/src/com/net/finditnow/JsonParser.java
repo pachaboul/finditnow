824af605debfe8f01180dab1e66fd1818bf3937e
Chanel Huang <Chanel Huang@localhost>
2011-03-03 20:56:19 -0800

    a fix to the crashing bug. Make sure the JsonObj the parser get is in fact a JsonObject

--- FindItNow/src/com/net/finditnow/JsonParser.java
+++ FindItNow/src/com/net/finditnow/JsonParser.java
@@ -22,7 +22,7 @@ public class JsonParser {
 	private static final String[] LOCATION_NAMES = { "lat",
 		"long",
 		"floor_names",
-	"name"};
+	"info"};
 
 	private static final String[] BUILDING_NAMES = { "bid",
 		"lat",
@@ -47,45 +47,48 @@ public class JsonParser {
 
 			for (int i = 0; i < arr.size(); i++)
 			{
-				//Since the JsonArray contains whole bunch json array, we can get each one out
-				JsonObject ob = arr.get(i).getAsJsonObject();
-
-				//some ways to get things out of a Json Object
-				/*System.out.println("Building Name: "+ob.get("buildingName").getAsString());
-					System.out.println("category Name: "+ob.get("category").getAsString());
-					System.out.println("floor Num: "+ob.get("floorNum").getAsInt());
-					System.out.println();*/
-
-				//place the information in the map with GeoPoint as key
-				GeoPoint point = new GeoPoint( ob.get(LOCATION_NAMES[0]).getAsInt(),ob.get(LOCATION_NAMES[1]).getAsInt());
-				if (ob.has(LOCATION_NAMES[2]))
+				if (arr.get(i).isJsonObject())
 				{
-					JsonArray s = ob.get(LOCATION_NAMES[2]).getAsJsonArray();
-					//the floor names associated with this point
-					String[] flrNames = gson.fromJson(s,String[].class);
-
-					//if the point is not added before, we add it
-					if (map.get(point) == null)
-						map.put(point,flrNames);
-					else
+					//Since the JsonArray contains whole bunch json array, we can get each one out
+					JsonObject ob = arr.get(i).getAsJsonObject();
+	
+					//some ways to get things out of a Json Object
+					/*System.out.println("Building Name: "+ob.get("buildingName").getAsString());
+						System.out.println("category Name: "+ob.get("category").getAsString());
+						System.out.println("floor Num: "+ob.get("floorNum").getAsInt());
+						System.out.println();*/
+	
+					//place the information in the map with GeoPoint as key
+					GeoPoint point = new GeoPoint( ob.get(LOCATION_NAMES[0]).getAsInt(),ob.get(LOCATION_NAMES[1]).getAsInt());
+					if (ob.has(LOCATION_NAMES[2]))
 					{
-						//if the point already has entries in map, we append it to the end.
-						String[] temp = map.get(point);
-						String[] newS = new String[temp.length+flrNames.length];
-						int c = 0;
-						for (; c < temp.length; c++)
-							newS[c] = temp[c];
-
-						for (int k = 0; k < flrNames.length; k++, c++)
+						JsonArray s = ob.get(LOCATION_NAMES[2]).getAsJsonArray();
+						//the floor names associated with this point
+						String[] flrNames = gson.fromJson(s,String[].class);
+	
+						//if the point is not added before, we add it
+						if (map.get(point) == null)
+							map.put(point,flrNames);
+						else
 						{
-							newS[c] = flrNames[k];
-
+							//if the point already has entries in map, we append it to the end.
+							String[] temp = map.get(point);
+							String[] newS = new String[temp.length+flrNames.length];
+							int c = 0;
+							for (; c < temp.length; c++)
+								newS[c] = temp[c];
+	
+							for (int k = 0; k < flrNames.length; k++, c++)
+							{
+								newS[c] = flrNames[k];
+	
+							}
+							map.put(point, newS);
 						}
-						map.put(point, newS);
 					}
+					else
+						map.put(point, new String[]{"n/a"});
 				}
-				else
-					map.put(point, new String[]{"n/a"});
 			}
 		} 
 		return map;
@@ -106,12 +109,15 @@ public class JsonParser {
 	
 			for (int i = 0; i < arr.size(); i++)
 			{
-				//have JsonObjects in the JsonArray, so get it out to process
-				JsonObject ob = arr.get(i).getAsJsonObject();
-	
-				//get the Geopoint and the name to put in map.
-				GeoPoint point = new GeoPoint( ob.get(LOCATION_NAMES[0]).getAsInt(),ob.get(LOCATION_NAMES[1]).getAsInt());
-				map.put(point,ob.get(LOCATION_NAMES[3]).getAsString());
+				if (arr.get(i).isJsonObject())
+				{
+					//have JsonObjects in the JsonArray, so get it out to process
+					JsonObject ob = arr.get(i).getAsJsonObject();
+		
+					//get the Geopoint and the name to put in map.
+					GeoPoint point = new GeoPoint( ob.get(LOCATION_NAMES[0]).getAsInt(),ob.get(LOCATION_NAMES[1]).getAsInt());
+					map.put(point,ob.get(LOCATION_NAMES[3]).getAsString());
+				}
 			}
 
 		} 
@@ -131,30 +137,33 @@ public class JsonParser {
 
 		for (int i = 0; i < arr.size(); i++)
 		{
-			//Since the JsonArray contains whole bunch json array, we can get each one out
-			JsonObject ob = arr.get(i).getAsJsonObject();
-
-			//some ways to get things out of a Json Object
-			/*System.out.println("Building Name: "+ob.get("buildingName").getAsString());
-				System.out.println("category Name: "+ob.get("category").getAsString());
-				System.out.println("floor Num: "+ob.get("floorNum").getAsInt());
-				System.out.println();*/
-
-			//place the information in the map with BuildingID as key
-			//int bid = ob.get(BUILDING_NAMES[0]).getAsInt();
-			GeoPoint point = new GeoPoint( ob.get(BUILDING_NAMES[1]).getAsInt(),ob.get(BUILDING_NAMES[2]).getAsInt());
-			//	String name = ob.get(BUILDING_NAMES[3]).getAsString();
-			ob.remove(BUILDING_NAMES[1]);
-			ob.remove(BUILDING_NAMES[2]);
-
-			//Log.i("log_tag", ob.toString());
-
-			Building build = gson.fromJson(ob,Building.class);
-			//JsonArray floor_ids_raw = ob.get(BUILDING_NAMES[4]).getAsJsonArray();
-			//JsonArray floor_names = ob.get(BUILDING_NAMES[5]).getAsJsonArray();
-			//Building build = new Building(bid, name);
-			//Log.i("log_tag", point.toString() + "   " + build.toString());
-			map.put(point, build);
+			if (arr.get(i).isJsonObject())
+			{
+				//Since the JsonArray contains whole bunch json array, we can get each one out
+				JsonObject ob = arr.get(i).getAsJsonObject();
+	
+				//some ways to get things out of a Json Object
+				/*System.out.println("Building Name: "+ob.get("buildingName").getAsString());
+					System.out.println("category Name: "+ob.get("category").getAsString());
+					System.out.println("floor Num: "+ob.get("floorNum").getAsInt());
+					System.out.println();*/
+	
+				//place the information in the map with BuildingID as key
+				//int bid = ob.get(BUILDING_NAMES[0]).getAsInt();
+				GeoPoint point = new GeoPoint( ob.get(BUILDING_NAMES[1]).getAsInt(),ob.get(BUILDING_NAMES[2]).getAsInt());
+				//	String name = ob.get(BUILDING_NAMES[3]).getAsString();
+				ob.remove(BUILDING_NAMES[1]);
+				ob.remove(BUILDING_NAMES[2]);
+	
+				//Log.i("log_tag", ob.toString());
+	
+				Building build = gson.fromJson(ob,Building.class);
+				//JsonArray floor_ids_raw = ob.get(BUILDING_NAMES[4]).getAsJsonArray();
+				//JsonArray floor_names = ob.get(BUILDING_NAMES[5]).getAsJsonArray();
+				//Building build = new Building(bid, name);
+				//Log.i("log_tag", point.toString() + "   " + build.toString());
+				map.put(point, build);
+			}
 		}
 
 		return map;
