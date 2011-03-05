f0334ba746bb4cf3cd8b4e077766c778ffed5bb5
Raftysworld <Raftysworld@gmail.com>
2011-03-04 20:29:10 -0800

    Pushing cleaned up Create.java and Building.java files

--- FindItNow/src/com/net/finditnow/Create.java
+++ FindItNow/src/com/net/finditnow/Create.java
@@ -1,11 +1,19 @@
+/***
+ * Create.java by Eric Hare
+ * This class defines our Create/Add interface with the PHP layer
+ * It is called to add items to the database
+ */
+
 package com.net.finditnow;
 
+// Java imports for parsing strings and lists
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+// Apache necessary imports
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -13,30 +21,48 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.json.*;
 
+// Log import to store error messages
 import android.util.Log;
 
+// GeoPoint for location services
 import com.google.android.maps.GeoPoint;
 
 public class Create {
 	
-	/**This method makes a request across the network to the database sending
-	the current location and category
-	@return: a JSONArray if item locations sent from the database */
-	public static JSONArray sendToDB(String category, GeoPoint location, int fid, String special_info, String bb, String sc, String print) {
-		/*
-		   * HTTP Post request
-		   */
-		String data = "";
+	// A Constant representing the location of the Create.php file
+	public static final String CREATE_LOCATION = "http://cubist.cs.washington.edu/projects/11wi/cse403/RecycleLocator/FINsert/create.php";
+	
+	/**
+	 * This method processes a request to add an item to the database
+	 * It calls the Create.php file with the given parameters
+	 * 
+	 * @param category The category selected by the user
+	 * @param location The location of the object if it is not in a building
+	 * @param fid The floor ID of the object if it IS in a building
+	 * @param special_info Currently ignored: Include any special info
+	 * @param bb If category is school supplies, "bb" indicates blue books at location
+	 * @param sc If category is school supplies, "sc" indicates scantrons at location
+	 * @param print If category is school supplies, "print" indicates printing at location
+	 * 
+	 * @return A String indicating if the request was successful
+	 */
+	public static String sendToDB(String category, GeoPoint location, int fid, 
+								  String special_info, String bb, String sc, String print) {
+
+		// Initialize input stream and response variables
 	  	InputStream iStream = null;
-	  	JSONArray infoArray = null;
-	  	try{
+		String data = "";
+	  	
+		// Attempt to make the HTTPPOST to the given location
+	  	try {
 		        HttpClient httpclient = new DefaultHttpClient();
-		        HttpPost httppost = new HttpPost("http://cubist.cs.washington.edu/projects/11wi/cse403/RecycleLocator/FINsert/create.php");
-		  			
+		        HttpPost httppost = new HttpPost(CREATE_LOCATION);
+		  		
+		        // Initialize the array of name value pairs
 		        List<BasicNameValuePair> nameValuePairs = new ArrayList<BasicNameValuePair>();
 		        
+		        // Special case for supplies.  Handle the optional parameters as needed
 		        if (category.toLowerCase() == "supplies") {
 		        	nameValuePairs.add(new BasicNameValuePair("category", "school_supplies"));
 		        	nameValuePairs.add(new BasicNameValuePair("bb", bb));
@@ -46,23 +72,27 @@ public class Create {
 		        	nameValuePairs.add(new BasicNameValuePair("category", category.toLowerCase()));
 		        }
 		        
-	  			nameValuePairs.add(new BasicNameValuePair("fid", fid+""));
+		        // Add the floor ID, and if not in a building, the lat and long
+	  			nameValuePairs.add(new BasicNameValuePair("fid", fid + ""));
 	  			if (fid == 0) {
 		  			nameValuePairs.add(new BasicNameValuePair("latitude", location.getLatitudeE6()+""));
 		  			nameValuePairs.add(new BasicNameValuePair("longitude", location.getLongitudeE6()+""));
 	  			}
+	  			
+	  			// Add any special information that we desire
 	  			nameValuePairs.add(new BasicNameValuePair("special_info", special_info));
 	  			
-	  			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-		        
+	  			// Process the response from the server
+	  			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));		        
 		        HttpResponse response = httpclient.execute(httppost);
 		        HttpEntity entity = response.getEntity();
 		        iStream = entity.getContent();
-	  	}catch(Exception e){
-	  	    Log.e("log_tag", "Error in http connection "+e.toString());
+	  	} catch(Exception e) {
+	  	    Log.e("log_tag", "Error in http connection " + e.toString());
 	  	}
-	  	//convert response to string
-	  	try{
+	  	
+	  	// Convert server's response to a String
+	  	try {
 		        BufferedReader reader = new BufferedReader(new InputStreamReader(iStream,"iso-8859-1"),8);
 		        StringBuilder sb = new StringBuilder();
 		        String line = null;
@@ -72,22 +102,10 @@ public class Create {
 		        iStream.close();
 		 
 		        data = sb.toString();
-		        Log.v("test", data);
-	  	}catch(Exception e){
-	  	    Log.e("log_tag", "Error converting result "+e.toString());
+	  	} catch(Exception e) {
+	  	    Log.e("log_tag", "Error converting result " + e.toString());
 	  	}
-	  	//Log.i("log_tag", "the output of request is : "+data);
-	  	try {
-		  	if (category == null) {
-		  		JSONTokener jsontok = new JSONTokener(data);
-		  		infoArray = CDL.rowToJSONArray(jsontok);
-		  	} else {
-		  		infoArray = new JSONArray(data);
-		  	}
-		} catch (JSONException e) {
-			Log.e("log_tag", "Error converting response to JSON "+e.toString());
-		}
-		
-	  	return infoArray;
+	  	
+	  	return data;
 	}
 }
