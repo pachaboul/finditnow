988628f6a5ffc80f1a7f4e0846eb54ea7ad372f9
Raftysworld <Raftysworld@gmail.com>
2011-03-04 21:18:12 -0800

    Finally, clean up FINMap.java and UWOverlay.java

--- FindItNow/src/com/net/finditnow/FINMap.java
+++ FindItNow/src/com/net/finditnow/FINMap.java
@@ -1,17 +1,25 @@
-/** FINMap.java
- *  This is MapView class which uses the Google Maps API
- *  It draws the overlay, communicates with the database, and detects user location
+/***
+ * FINMap.java by Eric Hare
+ * This class handles the MapView activity of our application
+ * It includes user location detection and overlay support
+ * The Google Maps API is used to accomplish these tasks
  */
 
 package com.net.finditnow;
 
+// Java library imports
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.util.HashMap;
 import java.util.List;
+
+// JSONArray import
 import org.json.JSONArray;
+
+// Google Maps for Android imports
 import com.google.android.maps.*;
 
+// Other Android library dependencies
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
@@ -22,12 +30,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-/**
- * This is the Map class, which integrates the Google Maps API and itemized overlays
- * It also includes location-awareness and houses most of the shared data in the front-end
- * @author EricHare
- *
- */
 public class FINMap extends MapActivity {
 	
 	// Map and Location Variables
@@ -49,10 +51,13 @@ public class FINMap extends MapActivity {
 	private static HashMap<GeoPoint, String[]> geoPointFloorMap;
 	private static HashMap<GeoPoint,String> geoPointSpecialInfoMap;	
 	
+	// A constant representing the default location of the user
+	// Change this the coordinates of another campus if desired (defaults to UW Seattle)
 	public static final GeoPoint DEFAULT_LOCATION = new GeoPoint(47654799,-122307776);
 
-	/** Called when the activity is first created.
-     * 	It initializes the map layout, detects the user's category, and builds the map
+	/** 
+	 * Called when the activity is first created.
+     * It initializes the map layout, detects the user's category, and builds the map
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -61,12 +66,12 @@ public class FINMap extends MapActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map);
         
-        // And to get the item name for buildings, supplies:
+        // Get the item name for buildings, supplies:
         Bundle extras = getIntent().getExtras(); 
         category = extras.getString("category");
         itemName = extras.getString("itemName");
         
-        // Set breadcrumbing in titlebar.
+        // Set the Breadcrumb in the titlebar
         if (itemName == null) {
         	setTitle("FindItNow > " + FINUtil.capFirstChar(category));
         } else {
@@ -77,18 +82,25 @@ public class FINMap extends MapActivity {
         createMap();
         locateUser();
         
+        // Retrieve locations from the database and parse them
     	JSONArray listOfLocations = Retrieve.requestFromDB(category, itemName, DEFAULT_LOCATION);
     	geoPointFloorMap = JsonParser.parseJson(listOfLocations);
     	geoPointSpecialInfoMap = JsonParser.parseNameJson(listOfLocations);
-            
+        
+    	// Add these locations to the map view
     	placeOverlays();
     }
-    
+    /**
+     * Called when the activity is paused to disable the location services
+     */
     public void onPause() {
     	super.onPause();
     	locOverlay.disableMyLocation();
     }
     
+    /**
+     * Create the Android options menu
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -96,43 +108,21 @@ public class FINMap extends MapActivity {
         return true;
     }
     
-    public static BigDecimal distanceTo(GeoPoint point) {
-    	Location dest = new Location("Hi");
-    	Location curr = new Location("Bye");
-    	
-    	if (location != null && !location.equals(DEFAULT_LOCATION)) {
-	    	float latitude_curr = (float) (location.getLatitudeE6()*1E-6);
-	    	float longitude_curr = (float) (location.getLongitudeE6()*1E-6);
-	    	float latitude_dest = (float) (point.getLatitudeE6()*1E-6);
-	    	float longitude_dest = (float) (point.getLongitudeE6()*1E-6);
-	    	
-	    	curr.setLatitude(latitude_curr);
-	    	curr.setLongitude(longitude_curr);
-	    	dest.setLatitude(latitude_dest);
-	    	dest.setLongitude(longitude_dest);
-	    	
-	    	MathContext mc = new MathContext(2);
-	    	
-	    	return new BigDecimal(curr.distanceTo(dest) * 0.000621371192, mc);
-    	} else {
-    		return new BigDecimal(-1);
-    	}
-    }
-    
-    public static int walkingTime(BigDecimal distance) {
-    	// 20 mile / minute ?
-    	// 20 minute / mile?
-    	BigDecimal dec = new BigDecimal(25 * distance.doubleValue(), new MathContext(2));
-    	return dec.intValue();
-    }
-    
+    /**
+     * Expand and define the Android options menu
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
+        
+    	// Handle the item selected
         switch (item.getItemId()) {
+        
+        	// Return to the categories screen
 	        case R.id.categories_button:
                 startActivity(new Intent(this, FINMenu.class));
 	            return true;
+	            
+	        // Center the map on the user's location if it is possible
 	        case R.id.my_location_button:
 	        	if (location != null) {
 	        		mapController.animateTo(location);
@@ -140,15 +130,67 @@ public class FINMap extends MapActivity {
 	        		Toast.makeText(this, "Error: Could Not Detect Location", Toast.LENGTH_SHORT).show();
 	        	}
 	            return true;
+	            
+	        // Add a new location to the map
 	        case R.id.add_new_button:
 	        	startActivity(new Intent(this, FINAddNew.class));
 	            return true;
+	        
+	        // Open up our help documentation
 	        case R.id.help_button:
 	        	startActivity(new Intent(this, FINHelp.class));
 	            return true;
+	            
 	        default:
 	            return super.onOptionsItemSelected(item);
         }
+    }
+    
+    /**
+     * This method returns the distance from the user's current location to a given point
+     * It returns this in BigDecimal format for ease of processing
+     * 
+     * @param point A GeoPoint corresponding to the location under consideration
+     * 
+     * @return A BigDecimal representing the distance to this point in miles
+     */
+    public static BigDecimal distanceTo(GeoPoint point) {
+    	
+    	// Define a math context and two location variables to process
+    	MathContext mc = new MathContext(2);
+    	Location curr = new Location("");
+    	Location dest = new Location("");
+    	
+    	// This method is valid so long as the location is not the default and not null
+    	if (location != null && !location.equals(DEFAULT_LOCATION)) {
+    		
+    		// Compute the latitude and longitude of the two points
+	    	// Add these values to our location variable
+	    	curr.setLatitude((float)(location.getLatitudeE6()*1E-6));
+	    	curr.setLongitude((float)(location.getLongitudeE6()*1E-6));
+	    	dest.setLatitude((float)(point.getLatitudeE6()*1E-6));
+	    	dest.setLongitude((float)(point.getLongitudeE6()*1E-6));
+	    	
+	    	// Return this value in miles rounded
+	    	return new BigDecimal(curr.distanceTo(dest) * 0.000621371192, mc);
+    	} else {
+    		
+    		// Return -1 if the location was not valid
+    		return new BigDecimal(-1);
+    	}
+    }
+    
+    /**
+     * This method computes the walking time for a given distance based on the mile time
+     * 
+     * @param distance The distance to calculate walking time over
+     * @param mile_time The amount of time in minutes to walk a mile
+     * 
+     * @return The walking time in minutes that it takes to walk the given distance rounded
+     */
+    public static int walkingTime(BigDecimal distance, int mile_time) {
+    	BigDecimal dec = new BigDecimal(mile_time * distance.doubleValue(), new MathContext(2));
+    	return dec.intValue();
     }
     
     /** This method creates the map and displays the overlays on top of it */
@@ -168,26 +210,34 @@ public class FINMap extends MapActivity {
         mapController.setZoom(17);
     }
     
-    /** This method locates the user and displays the user's location in an overlay icon */
+    /**
+     * This method locates the user and displays the user's location in an overlay icon
+     */
     private void locateUser() {
+    	
     	// Define a new LocationOverlay and enable it
         locOverlay = new MyLocationOverlay(this, mapView) {
+        	
+        	// Extend onLocationChanged() to add the result to the location variable
         	public void onLocationChanged(Location loc) {
         		super.onLocationChanged(loc);
         		location = new GeoPoint((int)(loc.getLatitude()*1E6), (int)(loc.getLongitude()*1E6));
         		mapOverlays.add(locOverlay);
         	}
         };
-        
-        // Enable the overlay
         locOverlay.enableMyLocation();
         
+        // Attempt to detect the user's current location
     	Toast.makeText(this, "Detecting Location, Please Wait...", Toast.LENGTH_SHORT).show();
     	location = locOverlay.getMyLocation();
     	
+    	// Define a handler to run the given commands after 3 seconds
     	Handler handler = new Handler();
     	handler.postDelayed(new Runnable() { 
+    		
+    		// Run this method after 3 seconds have elapsed
             public void run() { 
+            	
             	// Try to get the current location, otherwise set a default
         		if (location == null) {
         			Toast.makeText(FINMap.this, "Error: Could Not Detect Location", Toast.LENGTH_SHORT).show();
@@ -200,43 +250,64 @@ public class FINMap extends MapActivity {
         }, 3000);
     }
     
-    /** This method places the locations retrieved from the database onto the map */
+    /**
+     * This method places the locations retrieved from the database onto the map
+     */
     private void placeOverlays() {
     	
+    	// If the category is buildings, then we only put the single point on the map
     	if (getCategory().equals("buildings")) {
     		GeoPoint point = FINMenu.getGeoPointFromBuilding(itemName);
     		geoPointFloorMap.put(point, FINMenu.getBuilding(point).getFloorNames());
     	}
     	
+    	// Loop over the locations that we have retrieved and add them
         for (GeoPoint point : geoPointFloorMap.keySet()) {
-        	OverlayItem overlayItem = new OverlayItem(point, "blah", "blah");
+        	OverlayItem overlayItem = new OverlayItem(point, "", "");
         	itemizedOverlay.addOverlay(overlayItem);
         }
         
-        // Add our overlay to the list
+        // If we have retrieved items to display, add them to the overlay list
         if (!geoPointFloorMap.keySet().isEmpty()) {
         	mapOverlays.add(itemizedOverlay);
         }
     }
  
-    /** Required for Android Maps API compatibility */
+    /**
+     * Required for Android Maps API compatibility
+     */
     @Override
 	protected boolean isRouteDisplayed() {
 		return false;
 	}
     
-    /** This method returns the current category */
+    /**
+     * This method returns the category selected by the user
+     * @return A String representing the category chosen
+     */
     public static String getCategory() {
     	return category;
     }
 	
-	/** This method returns the floors associated with the location p */
-	public static String[] getFloors(GeoPoint p) {
-		return geoPointFloorMap.get(p);
+	/**
+	 * This method returns the floors where the location exists in the building
+	 * 
+	 * @param point The coordinates to retrieve this information from
+	 * 
+	 * @return A String array of floors where this location exists
+	 */
+	public static String[] getLocationFloors(GeoPoint point) {
+		return geoPointFloorMap.get(point);
 	}
 	
-	/** This method returns the location name associated with the location p */
-	public static String getSpecialInfo(GeoPoint p) {
-		return geoPointSpecialInfoMap.get(p);
+	/**
+	 * This method returns the special information associated with point
+	 * 
+	 * @param point The coordinates to retrieve this information from
+	 * 
+	 * @return A String representing the special information for the location
+	 */
+	public static String getSpecialInfo(GeoPoint point) {
+		return geoPointSpecialInfoMap.get(point);
 	}
 }
