015c1610bcc7bb6e7f3259d07ba38af837b3bebf
Chanel Huang <Chanel Huang@localhost>
2011-03-06 14:37:29 -0800

    Displays all floor names for every category, and only those that has the category on it has icon next to the name.

---
+++ FindItNow/tests/src/com/net/finditnow/test/BuildingTest.java
@@ -0,0 +1,57 @@
+package com.net.finditnow.test;
+
+import junit.framework.TestCase;
+import java.util.ArrayList;
+
+import android.util.Log;
+
+import com.net.finditnow.Building;
+import com.net.finditnow.FINUtil;
+
+public class BuildingTest extends TestCase {
+        
+	protected Building build;
+	
+	protected void setUp() {
+		int[] fid = new int[3];
+		fid[0] = 1;
+		fid[1] = 2;
+		fid[2] = 3;
+		
+		String[] fnames = new String[3];
+		fnames[0] = "Floor One";
+		fnames[1] = "Floor Two";
+		fnames[2] = "Floor Three";
+		
+		build = new Building(15, "Test Building", fid, fnames);
+	}
+	
+	public void testBID() {
+		assertTrue(build.getBuildingID() == 15);
+	}
+	
+	public void testName() {
+		assertTrue(build.getName().equals("Test Building"));
+	}
+	
+	public void testFID() {
+		int[] fid = new int[3];
+		fid[0] = 1;
+		fid[1] = 2;
+		fid[2] = 3;
+		
+		assertTrue(build.getFloorIDs()[1] == fid[1]);
+	}
+	
+	public void testFloorNames() {
+		String[] fnames = new String[3];
+		fnames[0] = "Floor One";
+		fnames[1] = "Floor Two";
+		fnames[2] = "Floor Three";
+		assertTrue(build.getFloorNames()[0].equals("Floor One"));
+	}
+	
+	public void testMap() {
+		assertTrue(build.floorMap().get("Floor One") == 1);
+	}
+}
