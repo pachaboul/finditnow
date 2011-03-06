70705cdd1cfa10623680a6b7c8a864aff814aeb2
Chanel Huang <Chanel Huang@localhost>
2011-03-05 16:01:26 -0800

    Fix crashing on Building show floor detail

---
+++ FindItNow/tests/src/com/net/finditnow/test/FINUtilTest.java
@@ -0,0 +1,42 @@
+package com.net.finditnow.test;
+
+import junit.framework.TestCase;
+import java.util.ArrayList;
+import com.net.finditnow.FINUtil;
+
+public class FINUtilTest extends TestCase {
+        
+	protected String string1;
+	protected String string2;
+	protected String string3;
+	protected ArrayList<String> stringArray1;
+	
+	protected void setUp() {
+		string1 = "cat";
+		string2 = "Cat";
+		string3 = "c";
+		
+		stringArray1 = new ArrayList<String>();		
+		stringArray1.add("chanel");
+		stringArray1.add("mai");
+	}
+	
+	public void test1() {
+	    assertTrue(FINUtil.capFirstChar(string1).equals("Cat"));
+	}
+	
+	public void test2() {
+	    assertTrue(FINUtil.capFirstChar(string2).equals("Cat"));
+	}
+	
+	public void test3() {
+	    assertTrue(FINUtil.capFirstChar(string3).equals("C"));
+	}
+	
+	public void test4() {
+		ArrayList<String> result4 = new ArrayList<String>();
+		result4.add("Chanel");
+		result4.add("Mai");
+	    assertTrue(FINUtil.capFirstChar(stringArray1).equals(result4));
+	}
+}
