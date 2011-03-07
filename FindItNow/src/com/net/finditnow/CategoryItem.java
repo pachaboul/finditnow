b158f7050b23d51fd13ce478ba5afbe16bdb80a6
Chanel Huang <Chanel Huang@localhost>
2011-03-07 11:44:07 -0800

    minor adjustment to PopUpDisplay scroll

---
+++ FindItNow/src/com/net/finditnow/CategoryItem.java
@@ -0,0 +1,99 @@
+package com.net.finditnow;
+
+/***
+ * CategoryItem.java by Chanel Huang
+ * This class defines a "CategoryItem" object which is a 
+ * convenient storage of information about a certain 
+ * category in regards with a location.
+ */
+
+import java.util.ArrayList;
+
+public class CategoryItem {
+
+	private ArrayList<String> floor_names;
+	private ArrayList<String> info;
+	private ArrayList<Integer> id;
+	
+	public CategoryItem(){
+		floor_names = new ArrayList<String>();
+		info = new ArrayList<String>();
+		id = new ArrayList<Integer>();
+	}
+
+	public ArrayList<String> getFloor_names() {
+		return floor_names;
+	}
+
+	public void addFloor_names(String floorNames) {
+		this.floor_names.add(floorNames);
+	}
+
+	public ArrayList<String> getInfo() {
+		return info;
+	}
+
+	public void addInfo(String info) {
+		this.info.add(info);
+	}
+
+	
+	public ArrayList<Integer> getId() {
+		return id;
+	}
+
+	public void addId(int id) {
+		this.id.add(id);
+	}
+
+
+	/**
+	 * auto-generated equals method
+	 * return true if two are equal, otherwise false
+	 * 
+	 * @param obj oth other to be compare to
+	 * @return boolean whether two object are equal or not
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		
+		CategoryItem other = (CategoryItem) obj;
+		if (this.floor_names.size() != other.floor_names.size())
+			return false;
+		for(int c= 0; c < floor_names.size(); c++)
+		{
+			if (! this.floor_names.get(c).equals(other.floor_names.get(c)) )
+				return false;
+		}
+		if (this.info.size() != other.floor_names.size())
+			return false;
+		for(int c= 0; c < floor_names.size(); c++)
+		{
+			if (! this.info.get(c).equals(other.info.get(c)) )
+				return false;
+		}
+		if (this.id.size() != other.id.size())
+			return false;
+		for(int c= 0; c < id.size(); c++)
+		{
+			if (this.id.get(c) != other.id.get(c) )
+				return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "CategoryItem [floor_names=" + floor_names + ", id=" + id
+				+ ", info=" + info + "]";
+	}
+	
+	
+	
+}
---
+++ FindItNow/tests/src/com/net/finditnow/test/JsonParserTest.java
@@ -0,0 +1,71 @@
+package com.net.finditnow.test;
+
+import junit.framework.TestCase;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import com.google.android.maps.GeoPoint;
+import com.net.finditnow.JsonParser;
+import com.net.finditnow.CategoryItem;
+
+public class JsonParserTest extends TestCase {
+        
+        protected String json1;
+        protected String json2;
+        
+        protected void setUp() {
+        	json1 ="[{\"id\":13,\"long\":-122309098,\"floor_names\":[],\"lat\":47656582,\"info\":\"Curbside 8, Siganos \n Mon to Fri, 10:30am to 3pm\"}," +
+			"{\"id\":14,\"long\":-122309098,\"floor_names\":[],\"lat\":47656582,\"info\":\"Hot Dawgs, Motosurf, Red Square BBQ \n Mon to Fri, 10:30am to 3pm\"}]";
+        	json2 = "[{\"id\":17,\"long\":-122309098,\"floor_names\":[\"Basement\"],\"lat\":47656582,\"info\":\"\"}]";       	
+        }
+        
+        public void testCategory1() {
+                HashMap<GeoPoint, CategoryItem> map = JsonParser.parseCategoryJson(json1);
+                HashMap<GeoPoint, CategoryItem> expected = new  HashMap<GeoPoint, CategoryItem>();
+                
+                CategoryItem value= new CategoryItem();
+                value.addFloor_names("");
+                value.addInfo("Curbside 8, Siganos \n Mon to Fri, 10:30am to 3pm");
+                value.addId(13);
+                value.addFloor_names("");
+                value.addInfo("Hot Dawgs, Motosurf, Red Square BBQ \n Mon to Fri, 10:30am to 3pm");
+                value.addId(14);
+
+                expected.put(new GeoPoint(47656582,-122309098), value);
+
+                assertTrue(categoryEquals(map,expected));
+        }
+        
+        public void testCategory2() {
+            HashMap<GeoPoint, CategoryItem> map = JsonParser.parseCategoryJson(json2);
+            HashMap<GeoPoint, CategoryItem> expected = new  HashMap<GeoPoint, CategoryItem>();
+            
+            CategoryItem value= new CategoryItem();
+            value.addFloor_names("Basement");
+            value.addInfo("");
+            value.addId(17);
+            expected.put(new GeoPoint(47656582,-122309098), value);
+            
+            assertTrue(categoryEquals(map,expected));
+        }
+    
+        
+        private boolean categoryEquals(HashMap<GeoPoint,CategoryItem> a, HashMap<GeoPoint,CategoryItem> b) {
+                Iterator<HashMap.Entry<GeoPoint, CategoryItem>> iterA = a.entrySet().iterator();
+                Iterator<HashMap.Entry<GeoPoint, CategoryItem>> iterB = b.entrySet().iterator();
+                boolean result = a.size() == b.size();
+
+                while (iterA.hasNext() && iterB.hasNext()) {
+                    HashMap.Entry<GeoPoint,CategoryItem> entryA = iterA.next();
+                    HashMap.Entry<GeoPoint, CategoryItem> entryB = iterB.next();
+                    
+                    result = result && ( entryA.getKey().equals(entryB.getKey()) );
+                    CategoryItem strA = entryA.getValue();
+                    CategoryItem strB = entryB.getValue();
+
+                    result = result && ( strA.equals(strB)) ;                        
+                }
+                
+                return result;
+        }
+}
