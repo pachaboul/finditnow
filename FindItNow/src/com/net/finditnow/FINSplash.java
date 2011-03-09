7cbc036408e4d6eb174fa465e0f7c070e2a18aaa
Raftysworld <Raftysworld@gmail.com>
2011-03-09 15:34:24 -0800

    Remove unused imports

--- FindItNow/src/com/net/finditnow/FINSplash.java
+++ FindItNow/src/com/net/finditnow/FINSplash.java
@@ -1,12 +1,7 @@
 package com.net.finditnow;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.MotionEvent;
 
