c445ba22fc8dce7c39be0115c9705b94b01cf508
Raftysworld <Raftysworld@localhost>
2011-03-21 10:33:37 -0700

    Increase the timeout so that the app doesn't complain when used on a really slow connection

--- FindItNow/src/com/net/finditnow/Get.java
+++ FindItNow/src/com/net/finditnow/Get.java
@@ -82,12 +82,12 @@ public class Get {
 			HttpParams httpParameters = new BasicHttpParams();
 			
 			// Set the timeout in milliseconds until a connection is established.
-			int timeoutConnection = 3000;
+			int timeoutConnection = 5000;
 			HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
 			
 			// Set the default socket timeout (SO_TIMEOUT) 
 			// in milliseconds which is the timeout for waiting for data.
-			int timeoutSocket = 3000;
+			int timeoutSocket = 5000;
 			HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
 			
 			DefaultHttpClient httpClient = new DefaultHttpClient(httpParameters);
