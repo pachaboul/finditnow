ffc996c4c1891ed6ea8603e530c094ca6176c0d8
Raftysworld <Raftysworld@localhost>
2011-03-21 20:29:44 -0700

    New home class to support tabbed layout

---
+++ FindItNow/src/com/net/finditnow/FINHome.java
@@ -0,0 +1,11 @@
+package com.net.finditnow;
+
+import android.app.TabActivity;
+import android.os.Bundle;
+
+public class FINHome extends TabActivity {
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.home);
+	}
+}
