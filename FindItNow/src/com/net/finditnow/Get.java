c56a9df56c6e49ede9c0123641315f11f2de1a91
naancat <naancat@localhost>
2011-03-21 23:17:51 -0700

    add a method to JsonParser that parses a JSON string consist of many categories.

---
+++ FindItNow/src/com/net/finditnow/Get.java
@@ -0,0 +1,119 @@
+/***
+ * Get.java by Eric Hare
+ * This class defines our Retrieve Locations interface with the PHP layer
+ * It is called to retrieve items from the database, including categories and buildings
+ */
+
+package com.net.finditnow;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.google.android.maps.GeoPoint;
+
+public class Get {
+
+	// A Constant representing the location of the root of the get files
+	private static final String GET_LOCATIONS_ROOT = "http://yinnopiano.com/fin/";
+
+	/**
+	 * This method processes a request to retrieve locations from the database
+	 * It is used for plotting the locations properly on the map
+	 * 
+	 * @param category The category of items to retrieve
+	 * @param item If the category is supplies, the type of supplies to retrieve
+	 * @param location The location of the user making the request
+	 * 
+	 * @return A JSONArray of locations, categories, or buildings from the database
+	 */
+	public static String requestFromDB(String category, String item, GeoPoint location, Context context) {
+
+		// Initialize input stream and response variables
+		String data = "";
+		InputStream iStream = null;
+
+		// Attempt to make the HTTPPOST to the given location
+		// DESIGN PATTERN: Exceptions.  In Get/Update/Create, we catch any exception in PHP communication
+		//				   This also allows us to localize errors that occur during the process
+		try {
+			String suffix = (location == null? (category == null? "getCategories.php" : "getBuildings.php") : (category.contains(" ")? "getAllLocations.php" : "getLocations.php"));
+			
+			HttpPost httppost = new HttpPost(GET_LOCATIONS_ROOT + suffix);
+
+			// If the location is not null, this is a request for items in a category
+			if (location != null) {
+
+				List<BasicNameValuePair> nameValuePairs = new ArrayList<BasicNameValuePair>();		  			
+				nameValuePairs.add(new BasicNameValuePair("cat", category));
+				Log.v("asdfksadfilsd", category);
+
+				// If the itemName is not null, this is a request for school supplies
+				if (item != null) {
+					nameValuePairs.add(new BasicNameValuePair("item", item));
+				}
+
+				// Add the lat and long of the user's location
+				nameValuePairs.add(new BasicNameValuePair("lat", location.getLatitudeE6()+""));
+				nameValuePairs.add(new BasicNameValuePair("long", location.getLongitudeE6()+""));
+
+				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			}
+			HttpParams httpParameters = new BasicHttpParams();
+			
+			// Set the timeout in milliseconds until a connection is established.
+			int timeoutConnection = 5000;
+			HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
+			
+			// Set the default socket timeout (SO_TIMEOUT) 
+			// in milliseconds which is the timeout for waiting for data.
+			int timeoutSocket = 5000;
+			HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
+			
+			DefaultHttpClient httpClient = new DefaultHttpClient(httpParameters);
+			HttpResponse httpResponse = httpClient.execute(httppost);
+			
+			HttpEntity entity = httpResponse.getEntity();
+			iStream = entity.getContent();
+		} catch(Exception e) {
+			Log.e("log_tag", "Error in http connection " + e.toString());
+		}
+
+		// Attempt to convert the response into a string
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(iStream,"iso-8859-1"),8);
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			iStream.close();
+
+			data = sb.toString();
+		} catch(Exception e) {
+			Log.e("log_tag", "Error converting result " + e.toString());
+			return context.getString(R.string.timeout);
+		}
+
+		//if category if null, it meant we're retrieving the list of categories
+		//slightly modify it to make it a JSON representation
+		Log.i("WTF", data);
+
+		return data.trim();
+	}
+}
---
+++ FindItNow/src/com/net/finditnow/JsonParser.java
@@ -0,0 +1,254 @@
+package com.net.finditnow;
+
+/***
+ * JsonParser.java by Chanel Huang
+ * This class provides methods to parse a Json string into a HashMap
+ * 
+ * Json String:
+ * [ {"lat": int, "long":int, "floor_names":[strings], "name":string} ,...]
+ * 
+ * 
+ */
+
+//packages for handling JSON
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.util.Log;
+
+import com.google.android.maps.GeoPoint;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonStreamParser;
+public class JsonParser {
+	 /*
+	 * Design Principle: Information Hiding
+	 * These two arrays are only visible to this class.  Other module do not
+	 * ever need to know the exact names of each data coming from the
+	 * back-end database via JSON objects.
+	 */
+	//This is a string to keep track of the names of each piece of information in the
+	//JSON array.
+	private static final String[] LOCATION_NAMES = { "lat",
+		"long",
+		"floor_names",
+		"info",
+		"id",
+		"cat"};
+	private static final String[] BUILDING_NAMES = { "bid",
+		"lat",
+		"long",
+		"name",
+		"fid",
+	"floor_names"};
+	
+	/**
+	 * parse a json string into a map of GeoPoint to Building
+	 *  
+	 * @param json the json string representation of an array of building objects
+	 * @return a map of location to its corresponding building object
+	 */
+	public static HashMap<GeoPoint, Building> parseBuildingJson(String json)
+	{
+		//used for parsing the JSON object
+		Gson gson = new Gson();
+		JsonStreamParser parser = new JsonStreamParser(json);
+		JsonArray arr = parser.next().getAsJsonArray();
+
+		//creates the map for information to be stored in
+		HashMap<GeoPoint,Building> map = new HashMap<GeoPoint,Building>();
+
+		for (int i = 0; i < arr.size(); i++)
+		{
+			if (arr.get(i).isJsonObject())
+			{
+				//Since the JsonArray contains whole bunch json array, we can get each one out
+				JsonObject ob = arr.get(i).getAsJsonObject();
+	
+				//place the information in the map with BuildingID as key
+				GeoPoint point = new GeoPoint( ob.get(BUILDING_NAMES[1]).getAsInt(),ob.get(BUILDING_NAMES[2]).getAsInt());
+	
+				//remove lat, long so it can be used for the Gson.fromJson
+				ob.remove(BUILDING_NAMES[1]);
+				ob.remove(BUILDING_NAMES[2]);
+	
+				//Log.i("log_tag", ob.toString());
+	
+				//converts a Json string directly to a building object
+				Building build = gson.fromJson(ob,Building.class);
+				
+				//puts it in the map
+				map.put(point, build);
+			}
+		}
+
+		return map;
+	}
+
+	/**
+	 *  parses a String of category names and parse it into ArrayList
+	 *  
+	 * @param json the string representation of an array of categories
+	 * @return an ArrayList of category names
+	 */
+	public static ArrayList<String> getCategoriesList(String json){
+		
+		String[] arr = json.split(",");
+		ArrayList<String> result = new ArrayList<String>();
+
+		for(int i = 0; i < arr.length; i++)
+		{
+				result.add(arr[i]);
+		}
+		Log.i("WTF", result.toString());
+		return result;
+	}
+	
+	/**
+	 * parses a Json Array into a map of locations and its corresponding CategoryItem for one category
+	 * 
+	 * @param jsonArray jsonArray containing information
+	 * @return HashMap<GeoPoint, CategoryItem> maps a location with its information
+	 */
+	public static HashMap<GeoPoint, CategoryItem> parseCategoryJson(String json)
+	{
+		//creates the map for information to be stored in
+		HashMap<GeoPoint,CategoryItem> map = new HashMap<GeoPoint,CategoryItem>();
+		
+		if (json != null && !json.equals("")) {
+
+			//String json = jsonArray.toString();
+			//used for parsing the JSON object
+			Gson gson = new Gson();
+			JsonStreamParser parser = new JsonStreamParser(json);
+			JsonArray arr = parser.next().getAsJsonArray();
+
+			for (int i = 0; i < arr.size(); i++)
+			{
+				if (arr.get(i).isJsonObject())
+				{
+					//Since the JsonArray contains whole bunch json array, we can get each one out
+					JsonObject ob = arr.get(i).getAsJsonObject();
+	
+					//place the information in the map with GeoPoint as key
+					GeoPoint point = new GeoPoint( ob.get(LOCATION_NAMES[0]).getAsInt(),ob.get(LOCATION_NAMES[1]).getAsInt());
+					CategoryItem item = new CategoryItem();
+
+					//if the point is already in the map, get it out to add to it
+					if (map.get(point) != null)
+					{
+						item = map.get(point);
+					}
+					
+					
+					if (ob.has(LOCATION_NAMES[2]))
+					{
+						JsonArray s = ob.get(LOCATION_NAMES[2]).getAsJsonArray();
+						//the floor names associated with this point
+						String[] flrNames = gson.fromJson(s,String[].class);
+						if (flrNames.length == 0)
+							item.addFloor_names("");
+						for (String flr: flrNames)
+							item.addFloor_names(flr);
+					}
+					if (ob.has(LOCATION_NAMES[3]))
+					{
+						String s = ob.get(LOCATION_NAMES[3]).getAsString();
+						//the floor info associated with this point
+						item.addInfo(s);
+					}
+					if (ob.has(LOCATION_NAMES[4]))
+					{
+						int id = ob.get(LOCATION_NAMES[4]).getAsInt();
+						//the floor id associated with this point
+						item.addId(id);
+					}
+
+					map.put(point, item);
+				}
+			}
+		} 
+		return map;
+	}
+	
+	/**
+	 * parses a Json Array into a map of locations and its corresponding CategoryItem for all category
+	 * 
+	 * @param jsonArray jsonArray containing information
+	 * @return HashMap<GeoPoint,HashMap<String,CategoryItem>> maps a location with its information
+	 */
+	public static HashMap<GeoPoint,HashMap<String,CategoryItem>> parseAllCategoryJson(String json)
+	{
+		//creates the map for information to be stored in
+		HashMap<GeoPoint,HashMap<String,CategoryItem>> map = new HashMap<GeoPoint,HashMap<String,CategoryItem>>();
+		
+		if (json != null && !json.equals("")) {
+
+			//used for parsing the JSON object
+			Gson gson = new Gson();
+			JsonStreamParser parser = new JsonStreamParser(json);
+			JsonArray arr = parser.next().getAsJsonArray();
+
+			for (int i = 0; i < arr.size(); i++)
+			{
+				if (arr.get(i).isJsonObject())
+				{
+					//Since the JsonArray contains whole bunch json array, we can get each one out
+					JsonObject ob = arr.get(i).getAsJsonObject();					
+					
+					//place the information in the map with GeoPoint as key
+					GeoPoint point = new GeoPoint( ob.get(LOCATION_NAMES[0]).getAsInt(),ob.get(LOCATION_NAMES[1]).getAsInt());
+					HashMap<String,CategoryItem> oneMap;
+
+					
+					//Grab the Map of Location if it is in it or make a new one
+					if (map.get(point) != null)
+					{
+						oneMap = map.get(point);
+					}
+					else{
+						oneMap = new HashMap<String,CategoryItem>();
+						map.put(point, oneMap);
+					}
+					
+					//grab the category and its corresponding CategoryItem if it is in the map
+					//else make a new one
+					String cat = ob.get(LOCATION_NAMES[5]).getAsString();
+					CategoryItem item;
+					if (oneMap.get(cat) != null){
+						item = oneMap.get(cat);
+					}else{
+						item = new CategoryItem();
+						oneMap.put(cat, item);
+					}
+
+					
+					if (ob.has(LOCATION_NAMES[2]))
+					{
+						JsonArray s = ob.get(LOCATION_NAMES[2]).getAsJsonArray();
+						//the floor names associated with this point
+						String[] flrNames = gson.fromJson(s,String[].class);
+						if (flrNames.length == 0)
+							item.addFloor_names("");
+						for (String flr: flrNames)
+							item.addFloor_names(flr);
+					}
+					if (ob.has(LOCATION_NAMES[3]))
+					{
+						String s = ob.get(LOCATION_NAMES[3]).getAsString();
+						//the floor info associated with this point
+						item.addInfo(s);
+					}
+					if (ob.has(LOCATION_NAMES[4]))
+					{
+						int id = ob.get(LOCATION_NAMES[4]).getAsInt();
+						//the floor id associated with this point
+						item.addId(id);
+					}
+				}
+			}
+		} 
+		return map;
+	}
+}
