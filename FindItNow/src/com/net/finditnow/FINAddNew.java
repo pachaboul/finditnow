4ec2422fe9588d84e790e9d17107601b98b6fe0c
Raftysworld <Raftysworld@localhost>
2011-03-23 15:23:43 -0700

    Clean up some unused imports

--- FindItNow/src/com/net/finditnow/FINAddNew.java
+++ FindItNow/src/com/net/finditnow/FINAddNew.java
@@ -8,7 +8,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.DialogInterface.OnMultiChoiceClickListener;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
