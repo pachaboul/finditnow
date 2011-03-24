9f16dd7778562cabf570e710b422f11e6f93c05c
Mai Dang <Mai Dang@localhost>
2011-03-23 23:05:46 -0700

    Added new tab drawables (work in progress), icons will be added soon

--- FindItNow/src/com/net/finditnow/FINHome.java
+++ FindItNow/src/com/net/finditnow/FINHome.java
@@ -1,10 +1,15 @@
 package com.net.finditnow;
 
 import android.app.TabActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class FINHome extends TabActivity {
@@ -12,23 +17,20 @@ public class FINHome extends TabActivity {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.home);
 	    
-	    Resources res = getResources(); 
-	    TabHost tabHost = getTabHost(); 
-	    TabHost.TabSpec spec; 
-	    Intent intent;  
+	    Resources res = getResources();
+	    TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
+	    tabHost.getTabWidget().setDividerDrawable(R.drawable.tab_divider);
 
-	    intent = new Intent().setClass(this, FINMenu.class);
-
-	    // Add Categories tab
-	    spec = tabHost.newTabSpec("categories").setIndicator("Categories", res.getDrawable(android.R.drawable.ic_menu_agenda)).setContent(intent);
-	    tabHost.addTab(spec);
-
-	    // Add Buildings tab
-	    intent = new Intent().setClass(this, BuildingList.class);
-	    spec = tabHost.newTabSpec("buildings").setIndicator("Buildings", res.getDrawable(android.R.drawable.ic_dialog_dialer)).setContent(intent);
-	    tabHost.addTab(spec);
-
-	    tabHost.setCurrentTab(0);
+	    addTab(new TextView(this),
+	    		"Categories",
+	    		res.getIdentifier("@android:drawable/ic_menu_agenda", null, getPackageName()),
+	    		tabHost,
+	    		new Intent().setClass(this, FINMenu.class));
+	    addTab(new TextView(this),
+	    		"Buildings",
+	    		res.getIdentifier("@android:drawable/ic_dialog_dialer", null, getPackageName()),
+	    		tabHost,
+	    		new Intent().setClass(this, BuildingList.class));
 	    
 	    if (getIntent().hasExtra("username")) {
 	    	Bundle extras = getIntent().getExtras(); 
@@ -41,5 +43,26 @@ public class FINHome extends TabActivity {
 			String result = extras.getString("result");
             Toast.makeText(getBaseContext(), result, Toast.LENGTH_LONG).show();
 	    }
+	}
+	
+	private void addTab(final View view, final String tag, final Integer id, TabHost host, Intent intent) {
+		View tabview = createTabView(host.getContext(), tag, id);
+		TabSpec setContent = host.newTabSpec(tag).setIndicator(tabview).setContent(intent);
+		host.addTab(setContent);
+	}
+		
+	private static View createTabView(final Context context, final String text, Integer imageID) {
+		View view = LayoutInflater.from(context).inflate(R.layout.tab_background, null);
+		
+		// Set up icon -- to be implemented later
+		/*
+		ImageView iv = (ImageView) view.findViewById(R.id.tabIcon);
+		iv.setImageResource(imageID);
+		*/
+		
+		// Set up label
+		TextView tv = (TextView) view.findViewById(R.id.tabLabel);
+		tv.setText(text);
+		return view;
 	}
 }
