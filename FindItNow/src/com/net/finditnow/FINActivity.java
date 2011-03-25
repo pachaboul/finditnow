62fb60ac5ed7a4b6d43d3f5505bd6bf81f6bc2d8
Raftysworld <Raftysworld@localhost>
2011-03-24 21:08:58 -0700

    Fix lots of miscellanous bugs

---
+++ FindItNow/src/com/net/finditnow/FINActivity.java
@@ -0,0 +1,97 @@
+package com.net.finditnow;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings.Secure;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class FINActivity extends Activity {
+	
+	/** 
+	 * Called when the activity is first created.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setTitle(getString(R.string.app_name));
+		
+	    if (getIntent().hasExtra("result")) {
+	    	Toast.makeText(getBaseContext(), getIntent().getExtras().getString("result"), Toast.LENGTH_LONG).show();
+	    }
+	}
+	
+	/**
+	 * Create the Android options menu
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.options_menu, menu);
+		return true;
+	}
+
+	/**
+	 * Expand and define the Android options menu
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		// Handle the item selected
+		switch (item.getItemId()) {
+
+		// Return to the categories screen
+		case R.id.home_button:
+			startActivity(new Intent(this, FINHome.class));
+			return true;
+		case R.id.login_button:
+			startActivity(new Intent(this, FINLogin.class));
+			return true;
+		case R.id.logout_button:
+			final String phone_id = Secure.getString(getBaseContext().getContentResolver(), Secure.ANDROID_ID);
+    		
+    		String result = DBCommunicator.logout(phone_id, getBaseContext());
+    		if (result.equals(getString(R.string.logged_out))) {
+    			FINHome.setLoggedIn(false);
+    		}
+    		
+    		Toast.makeText(getBaseContext(), result, Toast.LENGTH_LONG).show();
+    		
+    		return true;
+		case R.id.add_new_button:
+			if (FINHome.isLoggedIn()) {
+				startActivity(new Intent(this, FINAddNew.class));
+			} else {
+				Intent myIntent = new Intent(this, FINLogin.class);
+				myIntent.putExtra("result", getString(R.string.must_login));
+				
+				startActivity(myIntent);
+			}
+			return true;
+		case R.id.settings_button:
+			startActivity(new Intent(this, FINSettings.class));
+			return true;
+		case R.id.help_button:
+			startActivity(new Intent(this, FINHelp.class));
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		if (FINHome.isLoggedIn()) {
+			menu.findItem(R.id.login_button).setVisible(false);
+			menu.findItem(R.id.logout_button).setVisible(true);
+		} else {
+			menu.findItem(R.id.logout_button).setVisible(false);
+			menu.findItem(R.id.login_button).setVisible(true);
+		}
+
+		return true;
+	}
+}
---
+++ FindItNow/src/com/net/finditnow/FINAddIndoor.java
@@ -0,0 +1,133 @@
+package com.net.finditnow;
+
+import java.util.HashMap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings.Secure;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+public class FINAddIndoor extends FINActivity {
+
+	Building selectedBuilding;
+	String selectedFloor;
+	String selectedCategory;
+	boolean[] supplyTypes;
+	String special_info;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.addnew_indoor);
+		
+		// Set the text in the titlebar
+		setTitle(getString(R.string.app_name) + " > Add New Item > Indoor Item");
+
+		// Set up spinner for building selection
+		Spinner bSpinner = (Spinner) findViewById(R.id.addnew_bspinner);
+		ArrayAdapter<String> bAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, FINHome
+						.getBuildingsList());
+		bAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		bSpinner.setAdapter(bAdapter);
+		bSpinner.setOnItemSelectedListener(bspinner_listener);
+		selectedBuilding = FINHome.getBuilding(FINHome
+				.getGeoPointFromBuilding(FINHome.getBuildingsList().get(0)));
+
+		//Get category and types of school supplies from previous activity
+		Bundle extras = getIntent().getExtras();
+		selectedCategory = extras.getString("selectedCategory");
+		supplyTypes = extras.getBooleanArray("supplyTypes");
+		special_info = extras.getString("special_info");
+
+		//Set up "add item" button
+		Button addItem = (Button) findViewById(R.id.addnew_additem);
+		addItem.setOnClickListener(additem_listener);
+	}
+
+	//Listener for building spinner
+	private OnItemSelectedListener bspinner_listener = new OnItemSelectedListener() {
+		public void onItemSelected(AdapterView<?> parent, View arg1, int pos,
+				long arg3) {
+			selectedBuilding = FINHome.getBuilding(FINHome
+					.getGeoPointFromBuilding(parent.getItemAtPosition(pos)
+							.toString()));
+			//Set floor spinner accordingly
+			setFloorSpinner();
+		}
+
+		public void onNothingSelected(AdapterView<?> arg0) {
+			
+		}
+	};
+
+	//Listener for "add item" button
+	private OnClickListener additem_listener = new OnClickListener() {
+		public void onClick(View v) {
+			HashMap<String, Integer> map = selectedBuilding.floorMap();
+			
+			String bb = supplyTypes[0]? "bb" : "";
+			String sc = supplyTypes[1]? "sc" : "";
+			String pr = supplyTypes[2]? "print" : "";
+			String item = bb.length() > 0? "blue_book" : sc.length() > 0? "scantron" : pr.length() > 0? "printing" : "";
+			
+			//Send new item to database
+			final String phone_id = Secure.getString(getBaseContext().getContentResolver(), Secure.ANDROID_ID);
+			String result = DBCommunicator.create(phone_id, FINUtil
+					.deCapFirstChar(selectedCategory), map
+					.get(selectedFloor)+"", special_info, "", "", bb, sc, pr, getBaseContext());
+       	
+        	// Load the map with the new item
+	    	Intent myIntent = new Intent(getBaseContext(), FINMap.class);
+	    	myIntent.putExtra("result", result);
+	    	myIntent.putExtra("category", FINUtil.deCapFirstChar(selectedCategory));
+			myIntent.putExtra("building", "");
+			myIntent.putExtra("itemName", item);
+			myIntent.putExtra("centerLat", FINHome.getGeoPointFromBuilding(selectedBuilding.getName()).getLatitudeE6());
+			myIntent.putExtra("centerLon", FINHome.getGeoPointFromBuilding(selectedBuilding.getName()).getLongitudeE6());
+	    	
+            startActivity(myIntent);
+		}
+	};
+
+	//Listener for floor spinner
+	private OnItemSelectedListener fspinner_listener = new OnItemSelectedListener() {
+		public void onItemSelected(AdapterView<?> parent, View arg1, int pos,
+				long arg3) {
+			selectedFloor = parent.getItemAtPosition(pos).toString();
+		}
+
+		public void onNothingSelected(AdapterView<?> arg0) {
+		}
+	};
+
+	/**
+	 * Set up the spinner for selecting a floor based on current choice of building
+	 */
+	protected void setFloorSpinner() {
+		Spinner fSpinner = (Spinner) findViewById(R.id.addnew_fspinner);
+		ArrayAdapter<String> fAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, selectedBuilding
+						.getFloorNames());
+		fAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		fSpinner.setAdapter(fAdapter);
+		fSpinner.setOnItemSelectedListener(fspinner_listener);
+	}
+	
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		super.onPrepareOptionsMenu(menu);		
+		menu.findItem(R.id.add_new_button).setVisible(false);
+
+		return true;
+	}
+}
