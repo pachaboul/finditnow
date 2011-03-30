b3447f6e4a82c49d9810cf870220427321d91fb8
Raftysworld <Raftysworld@localhost>
2011-03-29 20:19:17 -0700

    Initial commit for region popup on first run of the app

---
+++ FindItNow/src/com/net/finditnow/FINSearch.java
@@ -0,0 +1,96 @@
+package com.net.finditnow;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+import com.google.android.maps.GeoPoint;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.SearchRecentSuggestions;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class FINSearch extends FINListActivity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.search);
+		
+		// Get the intent, verify the action and get the query
+		Intent intent = getIntent();
+		Bundle appData = getIntent().getBundleExtra(SearchManager.APP_DATA);
+
+		final String category = appData.getString("category");
+		final String itemName = appData.getString("itemName");
+		
+		setTitle(getString(R.string.app_name) + " > " + category + " > " + "Search");
+
+		if (Intent.ACTION_SEARCH.equals(intent.getAction()) && appData != null) {
+			String query = intent.getStringExtra(SearchManager.QUERY);
+
+			SearchRecentSuggestions suggestions = new SearchRecentSuggestions(this, SearchSuggestions.AUTHORITY, SearchSuggestions.MODE);
+			suggestions.saveRecentQuery(query, null);
+
+			String result = DBCommunicator.searchLocations(category, appData.getString("lat"), 
+					appData.getString("lon"), query, getBaseContext());
+
+			HashMap<Integer, GeoPoint> unsortedSearchMap = JsonParser.parseSearchJson(result);
+			ArrayList<GeoPoint> points = new ArrayList<GeoPoint>();
+			for (Integer id : unsortedSearchMap.keySet()) {
+				points.add(unsortedSearchMap.get(id));
+			}
+			Collections.sort(points, new ComparableGeoPoint());
+			final HashMap<Integer, GeoPoint> searchMap = new HashMap<Integer, GeoPoint>();
+			for (Integer id : unsortedSearchMap.keySet()) {
+				searchMap.put(id, points.get(id));
+			}
+			
+			ArrayList<String> foundLocations = new ArrayList<String>();
+			for (GeoPoint point : points) {
+				Building build = FINHome.getBuilding(point);
+				BigDecimal dist = FINMap.distanceBetween(FINMap.getLocation(), point);
+				String distance = dist.equals(new BigDecimal(-1))? "Cannot calculate" : dist + " mi.";
+				String str = (build == null? "Outdoor Location \n" : build.getName() + " \n")  + "Distance to here: " + distance + "\n" + 
+						FINMap.getCategoryItem(point, category).getInfo().get(0).replace("<br />", "\n");
+				foundLocations.add(str);
+			}
+
+			if (!points.isEmpty()) {
+
+				setListAdapter(new ArrayAdapter<String>(this, R.layout.building_list, foundLocations));
+
+				ListView lv = getListView();
+				lv.setTextFilterEnabled(true);
+
+				// Every item will launch the map
+				lv.setOnItemClickListener(new OnItemClickListener() {
+					public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+						GeoPoint selectedLoc = searchMap.get(position);
+
+						Intent myIntent = new Intent(v.getContext(), FINMap.class);
+
+						myIntent.putExtra("building", "");
+						myIntent.putExtra("category", category);
+						myIntent.putExtra("itemName", itemName);
+						myIntent.putExtra("centerLat", selectedLoc.getLatitudeE6());
+						myIntent.putExtra("centerLon", selectedLoc.getLongitudeE6());
+
+						startActivity(myIntent);
+					}
+				});
+			} else {
+				Toast.makeText(getBaseContext(), "No results found", Toast.LENGTH_SHORT).show();
+				finish();
+			}
+		}
+	}
+}
---
+++ FindItNow/src/com/net/finditnow/FINSplash.java
@@ -0,0 +1,106 @@
+package com.net.finditnow;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.PreferenceManager;
+import android.provider.Settings.Secure;
+import android.util.Log;
+import android.view.MotionEvent;
+
+public class FINSplash extends Activity {
+
+	protected boolean active = true;
+	protected int splashTime = 1500; // time to display the splash screen in ms
+	protected Thread splashThread;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// thread for displaying the SplashScreen
+		splashThread = new Thread() {
+
+			@Override
+			public void run() {
+
+				Intent myIntent = null;
+
+				// Check logged in status
+				final String phone_id = Secure.getString(getBaseContext().getContentResolver(), Secure.ANDROID_ID);
+
+				String loggedinstr = DBCommunicator.loggedIn(phone_id, getBaseContext());
+				String categories = DBCommunicator.getCategories(getBaseContext());
+				String buildings = DBCommunicator.getBuildings(getBaseContext());
+
+				boolean loggedin = loggedinstr.contains(getString(R.string.login_already));
+				boolean readytostart = !(loggedinstr.equals(getString(R.string.timeout)) || categories.equals(getString(R.string.timeout)) 
+									   || buildings.equals(getString(R.string.timeout)));
+
+				myIntent = new Intent(getBaseContext(), FINHome.class);
+				myIntent.addCategory("App Startup");
+				myIntent.putExtra("categories", categories);
+				myIntent.putExtra("buildings", buildings);
+				myIntent.putExtra("loggedin", loggedin);
+				myIntent.putExtra("readytostart", readytostart);
+
+				if (loggedin) {
+					myIntent.putExtra("username", loggedinstr.substring(21, loggedinstr.length()));
+				}
+				try {
+					int waited = 0;
+
+					while(active && (waited < splashTime)) {
+						sleep(100);
+						if (active) {
+							waited += 100;
+						}
+					}
+				} catch (InterruptedException e) {
+					// do nothing
+				} finally {
+					startActivity(myIntent);
+					finish();
+				}
+			}
+		};
+		
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);  
+		String listpref = prefs.getString("changeCampus", "");
+
+		if (listpref.equals("")) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Select your campus or region");
+			builder.setItems(getResources().getStringArray(R.array.campuses), campus_listener);		
+			builder.setCancelable(false);		
+	
+			AlertDialog alert = builder.create();
+			alert.show();
+		} else {
+			setContentView(R.layout.fin_splash);
+			splashThread.start();
+		}
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			active = false;
+		}
+		return true;
+	}
+	
+	// Listener for campus popup
+	private OnClickListener campus_listener = new OnClickListener() {
+
+		public void onClick(DialogInterface dialog, int which) {
+			setContentView(R.layout.fin_splash);
+			splashThread.start();
+		}
+	};
+}
