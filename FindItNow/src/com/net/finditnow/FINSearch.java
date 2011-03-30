b6eeac56bcae10f8555ad128e192322c37ea9652
Mai Dang <Mai Dang@localhost>
2011-03-29 21:50:44 -0700

    Changed search background to black again

--- FindItNow/src/com/net/finditnow/FINSearch.java
+++ FindItNow/src/com/net/finditnow/FINSearch.java
@@ -39,7 +39,6 @@ public class FINSearch extends FINListActivity {
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.search);
 		
 		// Get the intent, verify the action and get the query
 		Intent intent = getIntent();
