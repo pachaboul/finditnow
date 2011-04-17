3450d05e239cc96e5b094f54575e8fabccb69ae8
Raftysworld <Raftysworld@gmail.com>
2011-04-17 12:25:40 -0700

    Type to filter in the search box as per maddie's request. Implementation good?

--- FindItNow/src/com/net/finditnow/BuildingList.java
+++ FindItNow/src/com/net/finditnow/BuildingList.java
@@ -11,11 +11,14 @@ package com.net.finditnow;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -24,13 +27,20 @@ import com.google.android.maps.GeoPoint;
 public class BuildingList extends FINListActivity {
 
 	private ProgressDialog myDialog;
+	private EditText filterText;
+	private ArrayAdapter<String> adapter;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, FINHome.getBuildingsList()));
-
+		setContentView(R.layout.list_bg_with_filter);
+		
+		adapter = new ArrayAdapter<String>(this, R.layout.list_item, FINHome.getBuildingsList());
+		
+		filterText = (EditText) findViewById(R.id.search_box);
+	    filterText.addTextChangedListener(filterTextWatcher);
+	    
+		setListAdapter(adapter);
 		ListView lv = getListView();
 		lv.setTextFilterEnabled(true);
 
@@ -68,5 +78,27 @@ public class BuildingList extends FINListActivity {
 		menu.findItem(R.id.home_button).setVisible(false);
 
 		return true;
+	}
+	
+	private TextWatcher filterTextWatcher = new TextWatcher() {
+
+	    public void afterTextChanged(Editable s) {
+	    }
+
+	    public void beforeTextChanged(CharSequence s, int start, int count,
+	            int after) {
+	    }
+
+	    public void onTextChanged(CharSequence s, int start, int before,
+	            int count) {
+	        adapter.getFilter().filter(s);
+	    }
+
+	};
+
+	@Override
+	protected void onDestroy() {
+	    super.onDestroy();
+	    filterText.removeTextChangedListener(filterTextWatcher);
 	}
 }
