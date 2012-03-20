3b35086645bd9baf8855f5583ce2cd5f1a12f7b2
raftysworld <raftysworld@localhost>
2012-03-20 12:27:12 -0700

    Update the app to be compatible with the new server

--- FindItNow/src/com/net/finditnow/DBCommunicator.java
+++ FindItNow/src/com/net/finditnow/DBCommunicator.java
@@ -35,7 +35,6 @@ public class DBCommunicator {
 
 	// A Constant representing the location of FIN
 	private static final String FIN_ROOT = "http://www.fincdn.org/";
-	private static final String SECURE_FIN_ROOT = "https://www.project-fin.org/fin/";
 	private static final int CONNECTION_TIMEOUT = 5000;
 	private static final int SOCKET_TIMEOUT = 5000;
 
@@ -132,7 +131,7 @@ public class DBCommunicator {
 		nameValuePairs.add(new BasicNameValuePair("userpass", userpass));
 		nameValuePairs.add(new BasicNameValuePair("phone_id", phone_id));
 
-		return Post(SECURE_FIN_ROOT, "login.php", nameValuePairs, context);
+		return Post(FIN_ROOT, "login.php", nameValuePairs, context);
 	}
 
 	public static String loggedIn(String phone_id, Context context) {
